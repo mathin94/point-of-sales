cdd9a44b13949001333de08f30a7c07d7a0980b8
Mathin Mochammad <mathin.mochammad@gmail.com>
2021-08-14 20:28:39 +0700

    Setup Inertia

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Inertia\Inertia;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome');
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
